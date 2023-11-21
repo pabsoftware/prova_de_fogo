19390163b3ac9c59dfd0ad06ead6afb753051498
pabsoftware <pabsoftware@outlook.com>
2023-11-21 18:23:18 -0300

    configuração do gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles() {
+    resultado = gulp.src('./src/styles/*.scss')
+                .pipe(sass({ outputStyle: 'compressed'}))
+                .pipe(gulp.dest('./dest/css'));
+    return resultado
+}
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+}
