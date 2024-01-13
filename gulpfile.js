f31811114dd36e967dd7835a9acfde141a1f7b9c
pabsoftware <pabsoftware@outlook.com>
2024-01-12 23:00:58 -0300

    Criada as secoes sobre, resumo e trailer

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ const sass = require('gulp-sass')(require('sass'));
 function styles() {
     resultado = gulp.src('./src/styles/*.scss')
                 .pipe(sass({ outputStyle: 'compressed'}))
-                .pipe(gulp.dest('./dest/css'));
+                .pipe(gulp.dest('./dist/css'));
     return resultado
 }
+
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
